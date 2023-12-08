1e9b08cff4031d4496e420ed4694994ac6f59c23
Ashley Favello <ashfavello@gmail.com>
2023-12-07 22:56:06 -0800

    Fixed title

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -8,7 +8,7 @@ export function Header() {
         <img src={logo} alt="logo"></img>
       </a>
       <div>
-        <h1>Artist's Top 10 Tracks</h1>
+        <h1>Artist's Top Hits</h1>
         <p>Are you curious about an artist's best hits? Search their name and let's find out!</p>
       </div>
     </div>
